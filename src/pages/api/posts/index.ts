c7a5267a32b1b90788f93c29de75811448f7645e
Daniel González Briseño <qbixmex@gmail.com>
2024-10-02 15:39:25 -0700

    SSR Adapters

--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -1,7 +1,11 @@
 import type { APIRoute } from "astro";
 import { getCollection } from "astro:content";
 
+export const prerender = false;
+
 export const GET: APIRoute = async ({ params, request }) => {
+
+  console.log("REQUEST:", request);
 
   const posts = await getCollection("blog");
 
