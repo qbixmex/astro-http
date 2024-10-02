90391100d86eaf9096d3f3914c7f4341f3fe857c
Daniel González Briseño <qbixmex@gmail.com>
2024-10-02 16:11:58 -0700

    Optional Query Params

--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -1,11 +1,36 @@
 import type { APIRoute } from "astro";
-import { getCollection } from "astro:content";
+import { getCollection, getEntry } from "astro:content";
 
 export const prerender = false;
 
 export const GET: APIRoute = async ({ params, request }) => {
 
-  console.log("REQUEST:", request);
+  const url = new URL(request.url);
+  const slug = url.searchParams.get("slug");
+
+  
+  if (slug) {
+    const post = await getEntry("blog", slug);
+
+    if (!post) {
+      return new Response(JSON.stringify({
+        ok: false,
+        message: `Post not found with the given slug: ${slug}`,
+      }), {
+        status: 404,
+        headers: {
+          "Content-Type": "application/json"
+        },
+      });
+    }
+
+    return new Response(JSON.stringify(post), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json"
+      },
+    });
+  }
 
   const posts = await getCollection("blog");
 
