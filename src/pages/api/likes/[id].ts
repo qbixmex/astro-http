9c0ca3e2dd2aba3f4a14a0a2ab49c649946f3ee9
Daniel González Briseño <qbixmex@gmail.com>
2024-10-03 09:30:37 -0700

    Increment Likes

--- src/pages/api/likes/[id].ts
+++ src/pages/api/likes/[id].ts
@@ -3,7 +3,7 @@ import { db, eq, Posts } from "astro:db";
 
 export const prerender = false;
 
-export const GET: APIRoute = async ({ params, request }) => {
+export const GET: APIRoute = async ({ params }) => {
 
   const postId = params.id ?? "";
 
@@ -37,6 +37,47 @@ export const GET: APIRoute = async ({ params, request }) => {
     JSON.stringify({
       ok: true,
       post,
+    }),
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+};
+
+export const PUT: APIRoute = async ({ params, request }) => {
+
+  const postId = params.id ?? "";
+  const { likes = 0 } = await request.json();
+
+  const posts = await db
+    .select()
+    .from(Posts)
+    .where(eq(Posts.id, postId));
+
+  if (posts.length === 0) {
+    const newPost = {
+      id: postId,
+      title: "",
+      likes: 0,
+    };
+    await db.insert(Posts).values(newPost);
+    posts.push(newPost);
+  }
+
+  const post = posts.at(0)!;
+  post.likes = post.likes + likes;
+
+  await db
+    .update(Posts)
+    .set(post)
+    .where(eq(Posts.id, postId));
+
+  return new Response(
+    JSON.stringify({
+      ok: true,
     }),
     {
       status: 200,
