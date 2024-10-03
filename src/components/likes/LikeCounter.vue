508bdc5b9e91fcbcbfcdce5b3c808f590cd25604
Daniel González Briseño <qbixmex@gmail.com>
2024-10-03 09:52:38 -0700

    Like Component (vue)

---
+++ src/components/likes/LikeCounter.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="likePost">Like</button>
+</template>
+
+<script lang="ts" setup>
+  const likePost = () => {
+    console.log('Liked post 👍');
+  };
+</script>
+
+<style scoped>
+  button {
+    background-color: #5351bc;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 8px;
+    cursor: pointer;
+    transition: all 300ms ease-in-out;
+  }
+
+  button:hover {
+    background-color: #4a3f9a;
+  }
+</style>
