d3038d2ebd6287f3d1b02bbf09a9a43d94a42e21
Daniel González Briseño <qbixmex@gmail.com>
2024-10-02 21:54:12 -0700

    Create tables

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,25 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable } from 'astro:db';
+
+ const Clients = defineTable({
+  columns: {
+    id: column.number({
+      primaryKey: true,
+    }),
+    name: column.text(),
+    age: column.number(),
+    country: column.text(),
+    city: column.text(),
+    state: column.text(),
+    nationality: column.text(),
+    isActive: column.boolean({
+      default: false,
+    }),
+  },
+ });
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {
+    Clients,
+  }
 });
